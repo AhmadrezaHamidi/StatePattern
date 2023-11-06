7587948409fec5ea59ef7ee7843acf696d899b0b
ahmadhfr <vestaabner@gmail.com>
2023-11-06 11:11:58 +0330

    feat adding satet pattern s

---
+++ statePattern/Domain/Class1.cs
@@ -0,0 +1,52 @@
+﻿using Domain.States;
+
+namespace Domain;
+public class LoanRequest
+{
+public LoanReqestState State { get; private set; }
+public long Amount { get; private set; }
+public TimeSpan LoanDuration { get; private set; }
+
+    public LoanRequest()
+    {
+        this.State = new DraftSate();
+    }
+
+public void Update(long amount, TimeSpan duration) {
+
+
+        if (this.State.canUopdateReqest())
+    {
+        this.Amount = amount;
+        this.LoanDuration = duration;
+    }
+       
+    else
+    {
+        throw new Exception("!!!");
+    }
+
+}
+
+
+    public void Accept()
+    {
+
+        if (this.State.CanAccept()) {
+            this.State = new AcceptedtSate();
+        }
+        else {
+            throw new Exception();
+        }
+       // if(State == LoanRequestState.InProgress) {
+            //...
+         //   this.State = LoanRequestState.Accepted;
+       // }
+       // else {
+         //   throw new Exception("/////");
+        //}
+ }
+
+
+
+
---
+++ statePattern/Domain/States/LoanReqestState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Domain.States
+{
+	public abstract class LoanReqestState
+	{
+        public abstract bool canUopdateReqest();
+        public virtual bool CanAccept() => false;
+	}
+
+
+    public class DraftSate : LoanReqestState
+    {
+        public override bool canUopdateReqest() => true;
+    }
+
+
+    public class InProgressSate : LoanReqestState
+    {
+        public override bool canUopdateReqest() => false;
+        public override bool CanAccept() => true;
+
+    }
+
+    public class RejectedSate : LoanReqestState
+    {
+        public override bool canUopdateReqest() => false;
+
+    }
+    public class AcceptedtSate : LoanReqestState
+    {
+        public override bool canUopdateReqest() => false;
+
+    }
+
+}
+
